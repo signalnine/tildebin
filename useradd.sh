cd62c13e6e2488dba28eed086d7364f9587768ff
Gabe Ortiz <gortiz@ip-192-168-2-9.us-west-2.compute.internal>
2014-12-09 21:10:19 -0800

    added useradd.sh, a script to manage new users and ssh keys on a host list

---
+++ useradd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: useradd.sh [hostlist.txt] [username] [sshpubkey]'
+        exit 0
+    fi
+
+for i in `cat $1` 
+    do echo $i
+    ssh -oStrictHostKeyChecking=no $i "sudo useradd -s /bin/bash $2 ; sudo usermod -a -G sysadmin $2 ; sudo mkdir -p /home/$2/.ssh ; echo '$3' | sudo tee /home/$2/.ssh/authorized_keys ; sudo chown $2 /home/$2 -R" 
+done
