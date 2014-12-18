0f441d5a316866a24d8f662ec87b52b911010fb0
Gabe Ortiz <gabe@signalnine.net>
2014-12-17 16:11:21 -0800

    don't need group for sudo anymore

--- useradd.sh
+++ useradd.sh
@@ -6,5 +6,5 @@ if [[ $# -eq 0 ]] ; then
 
 for i in `cat $1` 
     do echo $i
-    ssh -oStrictHostKeyChecking=no $i "sudo useradd -s /bin/bash $2 ; sudo usermod -a -G sysadmin $2 ; sudo mkdir -p /home/$2/.ssh ; echo '$2 ALL=NOPASSWD: ALL' | sudo tee -a /etc/sudoers ; echo '$3' | sudo tee /home/$2/.ssh/authorized_keys ; sudo chown $2 /home/$2 -R" 
+    ssh -oStrictHostKeyChecking=no $i "sudo useradd -s /bin/bash $2 ; sudo mkdir -p /home/$2/.ssh ; echo '$2 ALL=NOPASSWD: ALL' | sudo tee -a /etc/sudoers ; echo '$3' | sudo tee /home/$2/.ssh/authorized_keys ; sudo chown $2 /home/$2 -R" 
 done
