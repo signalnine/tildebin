f7150bed95b7298f0d6e1f91b47f7f4170218867
Gabe Ortiz <gabe@signalnine.net>
2017-10-05 11:58:48 -0600

    Update emptysgs.py
    update region

--- emptysgs.py
+++ emptysgs.py
@@ -1,7 +1,7 @@
 # find security groups that aren't being utilized
 import boto
 import boto.ec2
-EC2_REGION='us-west-2'
+EC2_REGION='us-east-1'
 ec2region = boto.ec2.get_region(EC2_REGION)
 ec2 = boto.connect_ec2(region=ec2region)
 sgs = ec2.get_all_security_groups()
